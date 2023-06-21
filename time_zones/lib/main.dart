32751e692ba36e062dca17e01c21a73eea389219
Jakub Strakos <jakub.strakos@embedit.com>
2023-06-21 14:30:55 +0200

    feat: create learning app
    on the learning behalf, creating new app according to YT manual

--- time_zones/lib/main.dart
+++ time_zones/lib/main.dart
@@ -1,48 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: Card(),
     ));
 
-class Home extends StatelessWidget {
+class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('first app'),
+        title: Text('Card ID'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Row(
-        children: <Widget>[
-          Expanded(flex: 3, child: Image.asset('assets/peacock.jpg')),
-          Expanded(
-            flex: 3,
-            child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.cyan,
-                child: Text('1')),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.pinkAccent,
-                child: Text('2')),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Color.fromARGB(255, 56, 175, 45),
-                child: Text('3')),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: const Text('click'),
-        backgroundColor: Colors.red[500],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
     );
   }
